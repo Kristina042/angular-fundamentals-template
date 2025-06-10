21a84cbcd4eef00f6b591266721932902c64ee5f
Christine Miaekivi <christine.miaekivi@gmail.com>
2025-06-10 12:10:06 +0200

    implemented logic for a Star Wars application using RxJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {
   Subject,
   Subscription,
   switchMap,
+  debounceTime
 } from 'rxjs';
 import { MockDataService } from './mock-data.service';
 
@@ -34,7 +35,7 @@ export class AppComponent implements OnInit, OnDestroy {
     // 1.1. Add functionality to changeCharactersInput method. Changes searchTermByCharacters Subject value on input change.
     const inputValue: string = element.target.value;
     // YOUR CODE STARTS HERE
-
+    this.searchTermByCharacters.next(inputValue)
     // YOUR CODE ENDS HERE
   }
 
@@ -46,17 +47,26 @@ export class AppComponent implements OnInit, OnDestroy {
     // 3. Add debounce to prevent API calls until user stop typing.
 
     this.charactersResults$ = this.searchTermByCharacters
-        .pipe
+        .pipe(
         // YOUR CODE STARTS HERE
-
+        debounceTime(500),
+        filter(searchItem => searchItem.length > 2),
+        switchMap((searchItem) => this.mockDataService.getCharacters(searchItem))
         // YOUR CODE ENDS HERE
-        ();
+        );
+      
   }
 
   loadCharactersAndPlanet(): void {
     // 4. On clicking the button 'Load Characters And Planets', it is necessary to process two requests and combine the results of both requests into one result array. As a result, a list with the names of the characters and the names of the planets is displayed on the screen.
     // Your code should looks like this: this.planetAndCharactersResults$ = /* Your code */
     // YOUR CODE STARTS HERE
+    this.planetAndCharactersResults$ = forkJoin([
+      this.mockDataService.getCharacters(),
+      this.mockDataService.getPlanets()
+    ]).pipe(
+      map(arr => {return [...arr[0], ...arr[1]]})
+    );
     // YOUR CODE ENDS HERE
   }
 
@@ -67,12 +77,15 @@ export class AppComponent implements OnInit, OnDestroy {
     - Subscribe to changes
     - Check the received value using the areAllValuesTrue function and pass them to the isLoading variable. */
     // YOUR CODE STARTS HERE
+    combineLatest([this.mockDataService.getCharactersLoader(), this.mockDataService.getPlanetLoader()])
+    .subscribe((arr) => this.isLoading = this.areAllValuesTrue(arr))
     // YOUR CODE ENDS HERE
   }
 
   ngOnDestroy(): void {
     // 5.2 Unsubscribe from all subscriptions
     // YOUR CODE STARTS HERE
+    this.searchTermByCharacters.unsubscribe()
     // YOUR CODE ENDS HERE
   }
 
